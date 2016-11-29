0057d018aaaeb2d63c7ec257e33d32e98756b854
Gerry Ferdinandus <gerry_ferdinandus@hotmail.com>
2016-11-29 22:24:22 +0100

    add: call some function in driver.
    This will generate bigger project *.lst file

--- test/main.c
+++ test/main.c
@@ -1,7 +1,26 @@
+/*
+  Call some driver routine.
+*/
+
+#include "platform.h"
 
 int main(void);
 
 int main(void)
 {
-  return 0;
+    /* Initialize LCD */
+    lcd_initialize();
+
+    /* Clear LCD */
+    lcd_clear();
+
+    /* Display message on LCD */
+    lcd_display(LCD_LINE1, "  RENESAS   ");
+    lcd_display(LCD_LINE2, " YRDKRX63N  ");
+
+    for(;;)
+    {
+        //empty loop
+    }
+    return 0;
 }
